366e39701720b6af80dea895dd09069ee3d6733d
a044525499y <a044525499y@s09pc05>
2018-10-17 20:54:31 +0200

    comillas del json quitadas

--- src/main/java/net/daw/control/json.java
+++ src/main/java/net/daw/control/json.java
@@ -56,8 +56,8 @@ public class json extends HttpServlet {
 						TipousuarioService oService = new TipousuarioService(request);
 						try {
 							ReplyBean oReplyBean = oService.get();
-							strJson = "{\"status\":" + oReplyBean.getStatus() + ",\"message\":\"" + oReplyBean.getJson()
-									+ "\"}";
+							strJson = "{\"status\":" + oReplyBean.getStatus() + ",\"message\":" + oReplyBean.getJson()
+									+ "}";
 							
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
@@ -69,8 +69,8 @@ public class json extends HttpServlet {
 						TipousuarioService oService = new TipousuarioService(request);
 						try {
 							ReplyBean oReplyBean = oService.remove();
-							strJson = "{\"status\":" + oReplyBean.getStatus() + ",\"message\":\"" + oReplyBean.getJson()
-									+ "\"}";
+							strJson = "{\"status\":" + oReplyBean.getStatus() + ",\"message\":" + oReplyBean.getJson()
+									+ "}";
 							
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
